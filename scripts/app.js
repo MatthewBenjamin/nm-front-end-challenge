7eede7d39c7d0fdc0a2fc694f6cdb3f9f21ee835
Matthew Benjamin <matt.g.benjamin@gmail.com>
2016-10-09 13:08:34 -0500

    refactor: move youtube API call into main ViewModel

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,16 @@
-define(['jquery', 'knockout'], function($, ko) {
+define(['jquery', 'knockout', 'youtube'], function($, ko, youtube) {
     var ViewModel = function(){
         var self = this;
 
         self.videoResults = ko.observableArray();
         self.metaResults = ko.observable();
+
+        self.parseSearch = function (searchInput) {
+            var searchTerm = searchInput.query.value;
+            var videoResultsContainer = self.videoResults;
+            var metaResultsContainer = self.metaResults;
+            youtube.requestVideos(searchTerm, videoResultsContainer, metaResultsContainer);
+        };
     };
 
     return ViewModel;
--- scripts/kocomponents/search-form.js
+++ scripts/kocomponents/search-form.js
@@ -1,17 +1,6 @@
 // search-form.js
-define(['knockout', 'youtube', 'text!../../kotemplates/searchform.html'],
-    function(ko, youtube, htmlString) {
-    var searchForm = function(params) {
-        this.videoResults = params.videoResults;
-        this.metaResults = params.metaResults;
+define(['text!../../kotemplates/searchform.html'],
+    function(htmlString) {
 
-        this.parseSearch = function (searchInput) {
-            var searchTerm = searchInput.query.value
-            var videoResultsContainer = this.videoResults;
-            var metaResultsContainer = this.metaResults;
-            youtube.requestVideos(searchTerm, videoResultsContainer, metaResultsContainer);
-        };
-    };
-
-    return { viewModel: searchForm, template: htmlString }
+    return { template: htmlString }
 });
