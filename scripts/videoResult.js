dec5cc4286e69055737d39a9a4713e3a668fb50f
Matthew Benjamin <matt.g.benjamin@gmail.com>
2016-10-09 16:04:50 -0500

    feat: add content for currentVideoSelection

--- scripts/videoResult.js
+++ scripts/videoResult.js
@@ -1,30 +1,28 @@
 // videoResult.js
 define(['knockout'], function(ko) {
+    function parseDate(dateinfo) {
+        var dateObj = new Date(dateinfo);
+        return dateObj.toLocaleDateString();
+    }
+
+    function makeVideoUri(videoId) {
+        return "https://www.youtube.com/watch?v=" + videoId;
+    }
+
+    function makeChannelUri(channelId) {
+        return "https://www.youtube.com/channel/" + channelId;
+    }
+
     var videoResult = function(videoSnippet) {
-        this.videoId = ko.observable(videoSnippet.id.videoId);
-        this.channelId = ko.observable(videoSnippet.snippet.channelId);
+        this.videoLink = ko.observable(makeVideoUri(videoSnippet.id.videoId));
+        this.channelLink = ko.observable(makeChannelUri(videoSnippet.snippet.channelId));
         this.channelTitle = ko.observable(videoSnippet.snippet.channelTitle);
         this.description = ko.observable(videoSnippet.snippet.description);
-        this.publishedAt = ko.observable(videoSnippet.snippet.publishedAt);
-        this.thumbnails = ko.observable(videoSnippet.snippet.thumbnails);
+        this.dateInfo = ko.observable(new Date(videoSnippet.snippet.publishedAt));
+        this.publishDate = ko.observable(parseDate(videoSnippet.snippet.publishedAt));
+        this.smallThumbnail = ko.observable(videoSnippet.snippet.thumbnails.default.url);
+        this.mediumThumbnail = ko.observable(videoSnippet.snippet.thumbnails.medium.url);
         this.title = ko.observable(videoSnippet.snippet.title);
-        /* snippet:
-        .channelId #string
-        .channelTitle #string
-        .description #string
-        .publishedAt #time string
-        .thumbnails = {
-            default: {
-                height: #int
-                width: #int
-                url: #string
-            },
-            high: {},
-            medium: {},
-        }
-        .title #string
-
-        */
     };
 
     return videoResult;
