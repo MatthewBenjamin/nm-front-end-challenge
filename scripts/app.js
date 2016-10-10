aca6481bff0e0a9c2baf1afbb751af101085d4ac
Matthew Benjamin <matt.g.benjamin@gmail.com>
2016-10-10 00:37:57 -0500

    feat: responsive UI

--- scripts/app.js
+++ scripts/app.js
@@ -6,11 +6,23 @@ define(['jquery', 'knockout', 'youtube'], function($, ko, youtube) {
         self.metaResults = ko.observable();
         self.currentVideoSelection = ko.observable();
 
+        // mobile UI
+        self.mobileMenu = ko.observable(false);
+
+        self.toggleMobileMenu = function() {
+            if (self.mobileMenu()) {
+                self.mobileMenu(false);
+            } else {
+                self.mobileMenu(true);
+            }
+        };
+
         self.parseSearch = function (searchInput) {
             var searchTerm = searchInput.query.value;
             var videoResultsContainer = self.videoResults;
             var metaResultsContainer = self.metaResults;
             var currentVideoSelection = self.currentVideoSelection;
+
             youtube.requestVideos(searchTerm, videoResultsContainer,
                 metaResultsContainer, currentVideoSelection);
         };
