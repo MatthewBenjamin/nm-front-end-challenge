513e9c420c8e17720986b69aa76688ca8aa78403
Matthew Benjamin <matt.g.benjamin@gmail.com>
2016-10-10 14:58:05 -0500

    style: clean up

--- scripts/init.js
+++ scripts/init.js
@@ -1,14 +1,11 @@
 requirejs.config({
     baseUrl: 'scripts',
-    urlArgs: 'bust=' + (new Date()).getTime(),
     paths: {
         jquery: [
             'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min',
-            //'./libraries/jquery-2.1.3.min',
         ],
         knockout: [
             'http://ajax.aspnetcdn.com/ajax/knockout/knockout-3.3.0',
-            //'./libraries/knockout-3.3.0',
         ],
         app: 'app',
         // kocomponents
