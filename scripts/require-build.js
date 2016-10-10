ffd484e7bd280511501a42a2d6bd7dec44ae9a93
Matthew Benjamin <matt.g.benjamin@gmail.com>
2016-10-10 15:17:59 -0500

    chore: add r.js build profile

---
+++ scripts/require-build.js
@@ -0,0 +1,30 @@
+// app.build.js
+
+({
+    appDir: "../",
+    baseUrl: "scripts",
+    mainConfigFile: 'init.js',
+    dir: "../build",
+    modules: [
+        {
+            name: 'init',
+            include: [
+                'app',
+                'searchForm',
+                'resultsList',
+                'metaResults',
+                'currentVideoSelection',
+                'youtube',
+                'videoResult'
+            ],
+            findNestedDependencies: true,
+        },
+    ],
+    optimizeCss: "standard",
+    paths: {
+        jquery: 'empty:',
+        knockout: 'empty:',
+
+    },
+    removeCombined: true,
+});
