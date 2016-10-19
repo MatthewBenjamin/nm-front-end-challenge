0fffd7988c430521a398920b3719017492780598
Matthew Benjamin <matt.g.benjamin@gmail.com>
2016-10-19 15:27:15 -0500

    test: update view model tests

--- tests/spec/app-specs.js
+++ tests/spec/app-specs.js
@@ -1,7 +1,15 @@
 // component-spec.js
-describe('app', function() {
-    it('has a view model', function() {
-        expect(self).toBeTruthy();
+describe('view model', function() {
+    it('has defined observables', function() {
+        expect(self.searchInput).toBeDefined();
+        expect(self.searchPageToken).toBeDefined();
+        expect(self.videoResults).toBeDefined();
+        expect(self.metaResults).toBeDefined();
+        expect(self.currentVideoSelection).toBeDefined();
+        expect(self.errorMessage).toBeDefined();
+        expect(self.mobileMenu).toBeDefined();
+        expect(self.toggleMobileMenu).toBeDefined();
+        expect(self.performSearch).toBeDefined();
     });
 });
 
