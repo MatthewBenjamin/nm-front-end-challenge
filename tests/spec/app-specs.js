8325164a0326acaf860368c709980a452d206a6b
Matthew Benjamin <matt.g.benjamin@gmail.com>
2016-10-19 16:05:25 -0500

    test: implement jasmine w/ requirejs

--- tests/spec/app-specs.js
+++ tests/spec/app-specs.js
@@ -1,40 +1,41 @@
-// component-spec.js
-describe('view model', function() {
-    it('has defined observables', function() {
-        expect(self.searchInput).toBeDefined();
-        expect(self.searchPageToken).toBeDefined();
-        expect(self.videoResults).toBeDefined();
-        expect(self.metaResults).toBeDefined();
-        expect(self.currentVideoSelection).toBeDefined();
-        expect(self.errorMessage).toBeDefined();
-        expect(self.mobileMenu).toBeDefined();
-        expect(self.toggleMobileMenu).toBeDefined();
-        expect(self.performSearch).toBeDefined();
-    });
-});
-
-describe('youtube search', function() {
-    beforeEach(function(done) {
-        self.searchInput('puppies');
-        setTimeout(function() {
-            done();
-        }, 4500);
+define(['jasmineBoot'], function() {
+    describe('view model', function() {
+        it('has defined observables', function() {
+            expect(self.searchInput).toBeDefined();
+            expect(self.searchPageToken).toBeDefined();
+            expect(self.videoResults).toBeDefined();
+            expect(self.metaResults).toBeDefined();
+            expect(self.currentVideoSelection).toBeDefined();
+            expect(self.errorMessage).toBeDefined();
+            expect(self.mobileMenu).toBeDefined();
+            expect(self.toggleMobileMenu).toBeDefined();
+            expect(self.performSearch).toBeDefined();
+        });
     });
 
-    it('requests and retrieves search results', function() {
-        expect(self.videoResults().length).toBeTruthy();
-    });
+    describe('youtube search', function() {
+        beforeEach(function(done) {
+            self.searchInput('puppies');
+            setTimeout(function() {
+                done();
+            }, 4500);
+        });
 
-    it('parses video snippet data', function() {
-        var video = self.videoResults()[0];
-        expect(video.videoLink()).toBeDefined();
-        expect(video.channelLink()).toBeDefined();
-        expect(video.channelTitle()).toBeDefined();
-        expect(video.description()).toBeDefined();
-        expect(video.dateInfo()).toBeDefined();
-        expect(video.publishDate()).toBeDefined();
-        expect(video.smallThumbnail()).toBeDefined();
-        expect(video.mediumThumbnail()).toBeDefined();
-        expect(video.title()).toBeDefined();
+        it('requests and retrieves search results', function() {
+            expect(self.videoResults().length).toBeTruthy();
+        });
+
+        it('parses video snippet data', function() {
+            var video = self.videoResults()[0];
+            expect(video.videoLink()).toBeDefined();
+            expect(video.channelLink()).toBeDefined();
+            expect(video.channelTitle()).toBeDefined();
+            expect(video.description()).toBeDefined();
+            expect(video.dateInfo()).toBeDefined();
+            expect(video.publishDate()).toBeDefined();
+            expect(video.smallThumbnail()).toBeDefined();
+            expect(video.mediumThumbnail()).toBeDefined();
+            expect(video.title()).toBeDefined();
+        });
     });
 });
---
+++ tests/src/init.js
@@ -0,0 +1,49 @@
+requirejs.config({
+    baseUrl: '../scripts',
+    paths: {
+        jquery: [
+            'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min',
+        ],
+        knockout: [
+            'https://cdnjs.cloudflare.com/ajax/libs/knockout/3.4.0/knockout-min',
+        ],
+        // ko ViewModels
+        app: 'app',
+        videoResult: 'videoResult',
+        // ko components
+        searchForm: 'kocomponents/search-form',
+        resultsList: 'kocomponents/results-list',
+        metaResults: 'kocomponents/meta-results',
+        currentVideoSelection: 'kocomponents/current-video-selection',
+        // api
+        youtube: 'api/youtube',
+        // jasmine
+        jasmine: '../tests/lib/jasmine-2.5.2/jasmine',
+        jasmineHtml: '../tests/lib/jasmine-2.5.2/jasmine-html',
+        jasmineBoot: '../tests/lib/jasmine-2.5.2/boot',
+        // jasmine specs
+        appSpecs: '../tests/spec/app-specs',
+    },
+    shim: {
+        jasmineHtml: {
+            deps: ['jasmine'],
+        },
+        jasmineBoot: {
+            deps: [
+                'jasmine',
+                'jasmineHtml'
+            ],
+        }
+    }
+});
+
+// launch app w/ require dependencies
+require(['knockout', 'app', 'jasmineBoot', 'appSpecs'],
+    function (ko, ViewModel) {
+        ko.components.register('search-form', { require: 'searchForm' });
+        ko.components.register('results-list', { require: 'resultsList' });
+        ko.components.register('meta-results', { require: 'metaResults' });
+        ko.components.register('current-video-selection', { require: 'currentVideoSelection' });
+        ko.applyBindings(ViewModel);
+        window.onload()
+    });
